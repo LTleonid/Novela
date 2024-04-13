fa083da513216c824dcb0d1bcb669a904d82eda8
LT_NoteBook <klayray10568@gmail.com>
2024-04-13 19:43:01 +0700

    Switch -> for

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -1,15 +1,9 @@
 ﻿#include <iostream>
 #include <String>
 #include <windows.h>  
-
-#include "Voidless_Function.h"
-
+int counter = 0;
+using namespace std;
 
 int main() {
-	for (int i = 0; i < 30; i++) {
-		for (int j = 0; jы < 30; j++) {
-			cout << sprite1[i][j];
-		}
-	}
 	return 0;
 }
